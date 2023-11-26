414644cc57293d16b6d6a69ce7950e265535ba47
Tharanitharan <tharanitharan@guvi.in>
2023-11-26 11:34:10 +0530

    Constructor

--- OOP.md
+++ OOP.md
@@ -55,3 +55,33 @@ Name :  and Marks: 0
 
 '''
 ```
+
+
+## Constructor
+
+The construction method is called whenever a new object of that class is instantiated.
+
+```python
+
+class Student:
+
+    # constructor
+    def __init__(self, n, marks):
+        self.name = n
+        self.marks = marks
+
+    def display(self):
+        print(f"Name : {self.name}, and Marks: {self.marks}")
+
+student1 = Student("Suresh", 90)
+student1.display()
+
+student2 = Student("Ramesh", 80)
+student2.display()
+
+'''
+Output:
+Name : Suresh, and Marks: 90
+Name : Ramesh, and Marks: 80
+'''
+```
--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 class Student:
-    name = ""
-    marks = 0
+
+    # constructor
+    def __init__(self, n, marks):
+        self.name = n
+        self.marks = marks
 
     def display(self):
-        print(f"Name : {self.name} and Marks: {self.marks}\n")
+        print(f"Name : {self.name}, and Marks: {self.marks}")
 
-student1 = Student()
-student1.name = "Suresh"
-student1.marks = 90
+student1 = Student("Suresh", 90)
 student1.display()
 
-student2 = Student()
+student2 = Student("Ramesh", 80)
 student2.display()
 
 '''
 Output:
-
-Name : Suresh and Marks: 90
-
-Name :  and Marks: 0
-
+Name : Suresh, and Marks: 90
+Name : Ramesh, and Marks: 80
 '''
